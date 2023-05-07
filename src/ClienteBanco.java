0576c66b745fa903cb739b1b9a8cc8b4d9a63f9b
Leila Vasconcelos <leila.vasconcelos.022@gmail.com>
2023-05-07 14:47:13 -0300

    init project

---
+++ src/ClienteBanco.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class ClienteBanco {
+    public static void main(String[] args) {
+
+  /***********************
+  Dados iniciais do cliente:
+  Nome: Jackson da Silva
+  Tipo conta: Corrente
+  Saldo inicial: R$ 3000,00
+  ***********************/
+
+      Scanner leitor = new Scanner(System.in);
+
+      double saldoConta = 3000;
+      int numDigitado = 0;
+
+      String textoPadrao = """
+             
+              ------------------------
+              Operações
+                            
+              	1- Consultar saldos
+              	2- Receber valor
+              	3- Transferir valor
+              	4- Sair
+                            
+              Digite a opção desejada:
+                            """;
+
+      while (true){
+
+          System.out.println(textoPadrao);
+          numDigitado = leitor.nextInt();
+
+          if (numDigitado == 1){
+              System.out.println("O saldo da conta é: " + saldoConta);
+          }
+
+          if (numDigitado == 2) {
+              System.out.println("Informe o valor a receber:");
+              double valorReceber = leitor.nextInt();
+              saldoConta = saldoConta + valorReceber;
+          }
+
+          if (numDigitado == 3) {
+              System.out.println("Informe o valor a transferir:");
+              double valorTransferido = leitor.nextInt();
+
+              if ((valorTransferido > saldoConta) || (saldoConta == 0)) {
+                  System.out.println("A transferência não pode ser executada");
+              } else {
+                  System.out.println("Valor transferido");
+                  saldoConta = saldoConta - valorTransferido;
+              }
+          }
+
+          if ((numDigitado < 1) || (numDigitado > 4)){
+              System.out.println("Operação Inválida. Insira um número válido: ");
+          }
+
+          if (numDigitado == 4){
+              break;
+          }
+      }
+   }
+}
---
+++ src/Contador.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        int numDigitado = 0;
+        int contador = 1;
+        int numAleatorio = new Random().nextInt(100);
+        System.out.println(numAleatorio);
+
+        while ((numDigitado != numAleatorio) && (contador <= 5)) {
+
+            System.out.println("Digite um número: ");
+            Scanner leitor = new Scanner(System.in);
+            numDigitado = leitor.nextInt();
+
+            if (numDigitado == numAleatorio) {
+                System.out.println("*Este é o número correto! Parabéns!");
+                break;
+            }
+
+            if (numDigitado < numAleatorio) {
+                System.out.println("Esse numero é menor do que o numero correto!");
+            } else {
+                System.out.println("Esse número é maior do que o número correto!");
+            }
+
+            if (contador == 5) {
+                System.out.println(" ");
+                System.out.println("*Que pena que você não acertou! O número correto era: " + numAleatorio);
+            }
+
+            contador++;
+        }
+
+    }
+
+}
+
+
